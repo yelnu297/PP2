6e969edcfc68af324cbe1175f8e25c462cadfba8
Anu <a_salauatov@kbtu.kz>
2025-01-19 11:17:48 +0500

    jm

---
+++ Lab1/W3School/String.py
@@ -0,0 +1,15 @@
+a = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+print(a)
+
+b = "Hello, World!"
+print(b[-5:-2])
+
+d = "Hello"
+f = "World"
+c = d + " " + f
+print(c)
+
+txt = "We are the so-called \"Vikings\" from the north." 
