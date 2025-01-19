3f857952284b3097865874e2564ef26fdf85eb33
Anu <a_salauatov@kbtu.kz>
2025-01-19 10:57:02 +0500

    nm

---
+++ Lab 1/W3School/syntax.py
@@ -0,0 +1,9 @@
+if 5 > 2:
+ print("5 больше, 2 меньше!") 
+if 5 > 2:
+        print("5 больше, 2 меньше!") 
+"""
+ if 5 > 2:
+ print("Five is greater than two!")
+        print("Five is greater than two!") Syntax Error
+"""
