b52e1d6864a13f7457e57cbc86fd32484f413128
Anu <a_salauatov@kbtu.kz>
2025-01-28 00:12:22 +0500

    .

---
+++ Lab2/Operators.py
@@ -0,0 +1,12 @@
+print(10+2) #plus
+print(10-2) #minus
+print(10*2) #kobeitu
+print(10/2) #bolu
+print(10%2) #kaldygy
+print(10**2) #dareje
+print(10//2) #nakty san
+print(10&2) #AND
+print(10|2) #OR
+print(10^2) #XOR
+print(10>>2) #sdvig vpravo
+print(10<<2) #sdvig vlevo
