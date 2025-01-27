51b4a17fd2e9845f192f0bf44a68dca028272e3e
Anu <a_salauatov@kbtu.kz>
2025-01-28 02:50:43 +0500

    Add examples for looping through lists in loop_list.py and remove_list.py

---
+++ Lab2/Lists/loop_list.py
@@ -0,0 +1,6 @@
+thislist = ["apple", "banana", "cherry"]
+for i in range(len(thislist)):
+  print(thislist[i])
+
+thislist = ["apple", "banana", "cherry"]
+[print(x) for x in thislist] #apple, banana, cherry 
